bee9adf02b595ababf46e55661650640f53a23c7
lorenzodiaz2 <lorenzodiaz2002.l.d@gmail.com>
2025-11-26 14:17:51 +0100

    added some results and minor refactors

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
 from scalability.resolution_scalability import run_scalability
-from solvers.heuristic_solver import Heuristic_Solver
-from solvers.post_processing import Critical_Resources
-from utils.environment import Environment
-from utils.plot_functions import plot_paths
-from utils.results_io import read_scalability_results, read_files
 
 
-# run_scalability()
+
+
+run_scalability()
 # read_scalability_results()
 # read_files()
 
-env = Environment(20, 50, 5, 20, 0, 10)
-complete_solver = Heuristic_Solver(env.G, env.od_pairs)
-complete_solver.solve()
-complete_solver.assign_solutions()
-print(f"complete delay = {sum(a.delay for a in env.agents)}\n")
-
-env.compute_clusters()
-
-
-for cluster in env.clusters:
-    hs = Heuristic_Solver(env.G, [od_pair for od_pair in cluster.od_pairs])
-    hs.solve()
-    hs.assign_solutions()
-    # plot_paths(env.G, cluster.od_pairs)
-
-print(f"cluster delay = {sum(a.delay for a in env.agents)}\n")
-
-
-critical_resources = Critical_Resources(env.G, env.od_pairs)
-if critical_resources.is_initially_feasible:
-    print("Solution is already feasible.")
-else:
-    critical_resources.unassign_agents()
-    final_solver = Heuristic_Solver(env.G, env.od_pairs, critical_resources)
-    final_solver.solve()
-    final_solver.assign_solutions()
-    print(f"final delay = {sum(a.delay for a in env.agents)}\n")
+# env = Environment(20, 50, 5, 20, 0, 10)
+# complete_solver = Heuristic_Solver(env.G, env.od_pairs)
+# complete_solver.solve()
+# complete_solver.assign_solutions()
+# print(f"complete delay = {sum(a.delay for a in env.agents)}\n")
+#
+# env.compute_clusters()
+#
+#
+# for cluster in env.clusters:
+#     hs = Heuristic_Solver(env.G, [od_pair for od_pair in cluster.od_pairs])
+#     hs.solve()
+#     hs.assign_solutions()
+#
+# print(f"cluster delay = {sum(a.delay for a in env.agents)}\n")
+#
+#
+# critical_resources = Critical_Resources(env.G, env.od_pairs)
+# if critical_resources.is_initially_feasible:
+#     print("Solution is already feasible.")
+# else:
+#     critical_resources.unassign_agents()
+#     final_solver = Heuristic_Solver(env.G, env.od_pairs, critical_resources)
+#     final_solver.solve()
+#     final_solver.assign_solutions()
+#     print(f"final delay = {sum(a.delay for a in env.agents)}\n")
 
 
 
